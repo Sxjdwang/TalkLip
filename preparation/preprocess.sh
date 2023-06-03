397c42ec3395981f6d4399a7e2c562910c199f87
Sxjdwang <37468393+Sxjdwang@users.noreply.github.com>
2023-06-03 22:49:25 +0800

    Update preprocess.sh

--- preparation/preprocess.sh
+++ preparation/preprocess.sh
@@ -1,9 +1,1 @@
-#!/usr/bin/env bash
 
-export PATH=/workspace/jiadong/interpVSR:$PATH
-
-run.pl JOB=1:16 exp/decode.JOB.log \
-   python upvideo.py --split train --rank JOB --nshard 16
-
-python upvideo.py --split valid
-python upvideo.py --split test
